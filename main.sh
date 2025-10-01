2df09ba47a1d2501d295004efdea9c79b8d0da2f
Oliver12418 <106941247+Oliver12418@users.noreply.github.com>
2025-09-30 23:43:20 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,11 @@ sudo apt update
 sudo apt install nodejs npm git vsftpd -y
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install node
 nvm use node
 node -v
