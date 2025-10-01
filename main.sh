594c1c360a17b74adcbbe2cf41ca1ce157611c7b
Oliver12418 <106941247+Oliver12418@users.noreply.github.com>
2025-09-30 22:30:27 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,8 @@
 sudo apt update
 sudo apt install nodejs npm git vsftpd -y
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install node
+nvm use node
+node -v
+npm -v
